b4e526ba18bde63e582a60aa93bca753bdad015b
omarhashy <omarmagdyhashy@gmail.com>
2024-09-10 23:35:18 +0300

    add 404 page not found

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 
-
 const adminRoutes = require("./routes/admin");
 const shopRoutes = require("./routes/shop");
 
@@ -11,5 +10,10 @@ app.use(bodyParser.urlencoded({ extende: false }));
 
 app.use(adminRoutes);
 app.use(shopRoutes);
+
+app.use((req, res, next) => {
+  res.status(404);
+  res.send("<h1> Page not fiend</h1>");
+});
 
 app.listen(3000);
