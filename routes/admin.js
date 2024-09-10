cd475022d0660f20d3df19739691ec234b2f8b79
omarhashy <omarmagdyhashy@gmail.com>
2024-09-11 00:08:40 +0300

    Serving HTML Pages

--- routes/admin.js
+++ routes/admin.js
@@ -1,13 +1,10 @@
 const express = require("express");
+const path = require('path');
 
 const router = express.Router();
 
 router.get("/add-product", (req, res, next) => {
-  //   console.log("in another mid");
-  res.send(
-    "<form action = '/admin/product' method = 'POST'><input type = 'text' name = 'title'> <button type = 'submit'>Add Product</button></form>"
-  );
-  //   next();
+  res.sendFile(path.join(__dirname, "../", "views", "add-product.html"));
 });
 
 router.post("/product", (req, res, next) => {
--- routes/shop.js
+++ routes/shop.js
@@ -1,9 +1,12 @@
 const express = require("express");
+const path = require("path");
+
 const router = express.Router();
 
 router.get("/", (req, res, next) => {
   //   console.log("in another mid");
-  res.send("<h1>Hello from express</h1>");
+  //   res.send("<h1>Hello from express</h1>");
+  res.sendFile(path.join(__dirname, "../", "views", "shop.html"));
 });
 
-module.exports = router
+module.exports = router;
