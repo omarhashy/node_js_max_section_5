353e38756534915553c3a273646cac09cc0fa956
omarhashy <omarmagdyhashy@gmail.com>
2024-09-10 23:29:29 +0300

    make the routes file v2

--- app.js
+++ app.js
@@ -1,39 +1,13 @@
-// const http = require("http");
-// v1
-
 const express = require("express");
 const bodyParser = require("body-parser");
 
+
+const adminRoutes = require("./routes/admin");
+const shopRoutes = require("./routes/shop");
+
 const app = express();
 app.use(bodyParser.urlencoded({ extende: false }));
+app.use(adminRoutes);
+app.use(shopRoutes);
 
-// app.use((req, res, next) => {
-//   console.log("in the mid");
-//   next();
-// });
-//hi
-// app.use("/", (req, res, next) => {
-//   next();
-// });
-
-app.use("/add-product", (req, res, next) => {
-  //   console.log("in another mid");
-  res.send(
-    "<form action = '/product' method = 'POST'><input type = 'text' name = 'title'> <button type = 'submit'>Add Product</button></form>"
-  );
-  //   next();
-});
-
-app.post("/product", (req, res, next) => {
-  console.log(req.body);
-  res.redirect("/");
-});
-
-app.use("/", (req, res, next) => {
-  //   console.log("in another mid");
-  res.send("<h1>Hello from express</h1>");
-});
-
-// const server = http.createServer(app);
-// server.listen(3000);
 app.listen(3000);
