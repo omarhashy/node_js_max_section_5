9f7083400e574300229abe96cc6d7ed3e70c75cb
omarhashy <omarmagdyhashy@gmail.com>
2024-09-11 00:13:24 +0300

    Serving 404 HTML Page

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const path = require("path");
 
 const adminRoutes = require("./routes/admin");
 const shopRoutes = require("./routes/shop");
@@ -12,8 +13,7 @@ app.use("/admin", adminRoutes);
 app.use(shopRoutes);
 
 app.use((req, res, next) => {
-  res.status(404);
-  res.send("<h1> Page not fiend</h1>");
+  res.status(404).sendFile(path.join(__dirname , 'views','404.html'));
 });
 
 app.listen(3000);
