897f89974ecf47014eaebeb7308ad103f76e7419
omarhashy <omarmagdyhashy@gmail.com>
2024-09-11 02:10:18 +0300

    Installing & Implementing Pug

--- app.js
+++ app.js
@@ -2,19 +2,23 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const path = require("path");
 
-const adminRoutes = require("./routes/admin");
+const adminData = require("./routes/admin");
 const shopRoutes = require("./routes/shop");
 
 const app = express();
+//Set a default templating engine
+app.set("view engine", "pug");
+//Set a default views folder
+app.set("views", "views");
 
 app.use(bodyParser.urlencoded({ extende: false }));
 app.use(express.static(path.join(__dirname, "public")));
 
-app.use("/admin", adminRoutes);
+app.use("/admin", adminData.routes);
 app.use(shopRoutes);
 
 app.use((req, res, next) => {
   res.status(404).sendFile(path.join(__dirname, "views", "404.html"));
 });
 
-app.listen(3000);
+app.listen(8000);
--- routes/shop.js
+++ routes/shop.js
@@ -2,11 +2,15 @@ const express = require("express");
 const path = require("path");
 const rootDir = require("../util/path");
 const router = express.Router();
+const adminData = require("./admin");
 
 router.get("/", (req, res, next) => {
   //   console.log("in another mid");
   //   res.send("<h1>Hello from express</h1>");
-  res.sendFile(path.join(rootDir, "views", "shop.html"));
+  console.log(adminData.products);
+  // res.sendFile(path.join(rootDir, "views", "shop.html"));
+
+  res.render('shop');
 });
 
 module.exports = router;
