61a17b703ea45c9a7f5be5ce6e19c48b63ae91f9
omarhashy <omarmagdyhashy@gmail.com>
2024-09-12 02:14:47 +0300

    MVC Controllers 404

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const path = require("path");
 
 const adminRoutes = require("./routes/admin");
 const shopRoutes = require("./routes/shop");
+const errorController = require("./controllers/error");
 
 const app = express();
 
@@ -18,9 +19,6 @@ app.use(express.static(path.join(__dirname, "public")));
 app.use("/admin", adminRoutes);
 app.use(shopRoutes);
 
-app.use((req, res, next) => {
-  // res.status(404).sendFile(path.join(__dirname, "views", "404.html"));
-  res.render("404", { pageTitle: "Page not found" , path : '404'});
-});
+app.use(errorController.get404);
 
 app.listen(8000);
