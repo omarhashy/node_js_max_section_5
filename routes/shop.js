aab11a4f7f5bcb97fc6cfaa0969f1dedd8c9f360
omarhashy <omarmagdyhashy@gmail.com>
2024-09-14 19:32:20 +0300

    cart

--- routes/shop.js
+++ routes/shop.js
@@ -3,9 +3,13 @@ const shopController = require("../controllers/shop");
 
 const router = express.Router();
 router.get("/", shopController.getIndex);
-router.get("/products", shopController.getProducts);
+router.get("/checkout", shopController.getCheckout);
+router.get("/orders", shopController.getOrders);
 router.get("/cart", shopController.getCart);
-router.get("/checkout",shopController.getCheckout);
-router.get("/orders",shopController.getOrders);
+router.post('/cart',shopController.postCart)
+
+router.get("/products", shopController.getProducts);
+router.get("/products/:productId", shopController.getProduct);
+
 
 module.exports = router;
