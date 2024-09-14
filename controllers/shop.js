18c9d41d8ba448a64f891724011f65b69e832b9f
omarhashy <omarmagdyhashy@gmail.com>
2024-09-14 17:59:53 +0300

    section 9

--- controllers/shop.js
+++ controllers/shop.js
@@ -24,6 +24,11 @@ exports.getCart = (req, res, next) => {
   res.render("shop/cart", { path: "/cart", pageTitle: "Your Cart" });
 };
 
+
+exports.getOrders = (req, res, next) => {
+  res.render("shop/orders", { path: "/orders", pageTitle: "Orders" });
+};
+
 exports.getCheckout = (req, res, next) => {
   res.render("shop/checkout", { path: "/checkout", pageTitle: "Checkout" });
 };
